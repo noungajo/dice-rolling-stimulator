6cf5a5b9b7ecb3df7ecbd291a96f294ef11bdbe9
noutcha <jonathannoutcha@gmail.com>
2024-02-10 14:12:54 +0100

    looping

--- dicsimulator.py
+++ dicsimulator.py
@@ -4,45 +4,51 @@ import random
 random_number = random.randint(1,6)
 print(random_number)
 
-#check the number
-if random_number == 1:
-    print("---------")
-    print("|       |")
-    print("|   0   |")
-    print("|       |")
-    print("---------")
+repeat_again = 'y'
 
-if random_number == 2:
-    print("---------")
-    print("|       |")
-    print("|0     0|")
-    print("|       |")
-    print("---------")
+while repeat_again == 'y':
 
-if random_number == 3:
-    print("---------")
-    print("|   0   |")
-    print("|   0   |")
-    print("|   0   |")
-    print("---------")
+    #check the number
+    if random_number == 1:
+        #print the face
+        print("---------")
+        print("|       |")
+        print("|   0   |")
+        print("|       |")
+        print("---------")
 
-if random_number == 4:
-    print("---------")
-    print("|0     0|")
-    print("|       |")
-    print("|0     0|")
-    print("---------")
+    if random_number == 2:
+        print("---------")
+        print("|       |")
+        print("|0     0|")
+        print("|       |")
+        print("---------")
 
-if random_number == 5:
-    print("---------")
-    print("|0     0|")
-    print("|   0   |")
-    print("|0     0|")
-    print("---------")
+    if random_number == 3:
+        print("---------")
+        print("|   0   |")
+        print("|   0   |")
+        print("|   0   |")
+        print("---------")
 
-if random_number == 6:
-    print("---------")
-    print("|0     0|")
-    print("|0     0|")
-    print("|0     0|")
-    print("---------")
+    if random_number == 4:
+        print("---------")
+        print("|0     0|")
+        print("|       |")
+        print("|0     0|")
+        print("---------")
+
+    if random_number == 5:
+        print("---------")
+        print("|0     0|")
+        print("|   0   |")
+        print("|0     0|")
+        print("---------")
+
+    if random_number == 6:
+        print("---------")
+        print("|0     0|")
+        print("|0     0|")
+        print("|0     0|")
+        print("---------")
+        repeat_again = input("Do you want to continue ? y/n ")
