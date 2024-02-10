a7d31fff55d620fbbdff61e4f8a6debd6e116b91
noutcha <jonathannoutcha@gmail.com>
2024-02-10 14:08:47 +0100

    print the face

--- dicsimulator.py
+++ dicsimulator.py
@@ -12,3 +12,37 @@ if random_number == 1:
     print("|       |")
     print("---------")
 
+if random_number == 2:
+    print("---------")
+    print("|       |")
+    print("|0     0|")
+    print("|       |")
+    print("---------")
+
+if random_number == 3:
+    print("---------")
+    print("|   0   |")
+    print("|   0   |")
+    print("|   0   |")
+    print("---------")
+
+if random_number == 4:
+    print("---------")
+    print("|0     0|")
+    print("|       |")
+    print("|0     0|")
+    print("---------")
+
+if random_number == 5:
+    print("---------")
+    print("|0     0|")
+    print("|   0   |")
+    print("|0     0|")
+    print("---------")
+
+if random_number == 6:
+    print("---------")
+    print("|0     0|")
+    print("|0     0|")
+    print("|0     0|")
+    print("---------")
