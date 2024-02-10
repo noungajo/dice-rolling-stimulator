c66a050bde103a8f262429b5143b683a4d3d67d2
noutcha <jonathannoutcha@gmail.com>
2024-02-10 14:14:04 +0100

    change the generated random number while looping

--- dicsimulator.py
+++ dicsimulator.py
@@ -1,13 +1,13 @@
 import random
 
-#developping a random number
-random_number = random.randint(1,6)
-print(random_number)
+
 
 repeat_again = 'y'
-
+#looping
 while repeat_again == 'y':
-
+    #developping a random number
+    random_number = random.randint(1,6)
+    print(random_number)
     #check the number
     if random_number == 1:
         #print the face
