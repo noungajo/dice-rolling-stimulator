f1d393f20a496dbf19a4e8bdfcb78f85291f913e
noutcha <jonathannoutcha@gmail.com>
2024-02-10 14:08:13 +0100

    check number

--- dicsimulator.py
+++ dicsimulator.py
@@ -1,4 +1,14 @@
 import random
 
+#developping a random number
 random_number = random.randint(1,6)
 print(random_number)
+
+#check the number
+if random_number == 1:
+    print("---------")
+    print("|       |")
+    print("|   0   |")
+    print("|       |")
+    print("---------")
+
